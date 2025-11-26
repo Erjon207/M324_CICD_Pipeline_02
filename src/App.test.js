84e4b628a254d16d5d742df814e013b6a4c03efc
Erjon Thaqi <e3cthe@e3ag.net>
2025-11-26 22:04:50 +0100

    fix

--- src/App.test.js
+++ src/App.test.js
@@ -2,8 +2,8 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders heading text', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const heading = screen.getByText(/App Ref\. Card 02/i);
+  expect(heading).toBeInTheDocument();
 });
